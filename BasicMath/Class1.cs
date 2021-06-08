e4880fd256a55e00ecbb84bdf86c34e863303e9c
Nazar Boichuk <nazboichuk@gmail.com>
2021-06-08 11:09:20 +0300

    New Projects

---
+++ BasicMath/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicMath
+{
+	public class Class1
+	{
+		public int OperationWithStrings()
+		{
+			return ("Sample").Length;
+
+		}
+	}
+}
---
+++ CastingAndBoxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CastingAndBoxing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 100;
+			long b = a;
+
+			int i = 10;
+			object o = i;
+			long x = (int)o;
+			Console.WriteLine(x);
+		}
+	}
+}
