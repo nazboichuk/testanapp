e3ac41f9fea8ed0dd611e6c9199edaa702cca7a6
Nazar Boichuk <nazboichuk@gmail.com>
2021-06-09 13:20:27 +0300

    Move solutions

---
+++ OperationWithStrings/StringsInteroperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperationWithStrings
+{
+	public class StringsInteroperation
+	{
+		public int GetStringLength(string value)
+		{
+			return value.Length;
+		}
+
+		public bool DoesStringContainsOtherString(string s1, string s2)
+		{
+			return s1.Contains(s2);
+		}
+
+		public bool DoesStringEqualsToOtherString(string s1, string s2)
+		{
+			return s1.Equals(s2);
+		}
+
+		public string RemoveSpacesFromString(string value)
+		{
+			return value.Trim();
+		}
+
+		public string ConvertStringToUpperCase(string value)
+		{
+			return value.ToUpper();
+		}
+
+		public string ConvertStringToLowerCase(string value)
+		{
+			return value.ToLower();
+		}
+	}
+}
