3d15cac054f0b251b139b1d9b8fa9700813d3b85
nazar.boichuk-at-934627200577 <nboichuk@softserveinc.com>
2021-07-06 15:25:32 +0300

    HW_4 part 1

---
+++ EnterAndPrintString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EnterAndPrintString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// HW 1
+			// Enter the name of the drink (coffee, tea, juice, water). Print the name of the drink and its price.
+			PrintPriceForDrink();
+
+			// HW 2
+			// Enter a character string. Print each second character
+
+		}
+
+		public static void PrintPriceForDrink()
+		{
+			Console.WriteLine("Enter your drink name to buy:");
+			string name = Console.ReadLine();
+			switch (name)
+			{
+				case "Coffee":
+					Console.WriteLine("The price for the coffee: 4$");
+					break;
+				case "Tea":
+					Console.WriteLine("The price for the tea: 2$");
+					break;
+				case "Juice":
+					Console.WriteLine("The price for the juice: 5$");
+					break;
+				case "Water":
+					Console.WriteLine("The water is free");
+					break;
+				default:
+					Console.WriteLine("***** Drink not found ******");
+					break;
+			}
+		}
+	}
+}
