79b5bf6c355d7ff8f9aa354aa6cd2dbf68c85383
nazar.boichuk-at-934627200577 <nboichuk@softserveinc.com>
2021-07-18 21:53:49 +0300

    update

--- TriangleAreaCalculator/Program.cs
+++ TriangleAreaCalculator/Program.cs
@@ -8,10 +8,11 @@ namespace TriangleAreaCalculator
 		{
 			// створити консольну аплікацію, ка обчислює площу трикутника 2 способами:
 			// формула Герона, і формула 1/2 висоти трикутника помножена сторону до якої опущена
-			CalculateTriangleArea(3, 5);
+			//CalculateTriangleArea(3, 5);
+			CalculateTriangleArea();
 		}
 
-		public static void CalculateTriangleArea(double A, double B, double C)
+		/*public static void CalculateTriangleArea(double A, double B, double C)
 		{
 			// Enter Gerone
 			Console.WriteLine("#1 Enter the size of the sides of triangle:");
@@ -26,6 +27,29 @@ namespace TriangleAreaCalculator
 			Console.WriteLine("#2 - Gerone - Enter the size of the sides of triangle:");
 			double S = (A * H)/2;
 			Console.WriteLine(S);
+		}*/
+		
+		public static void CalculateTriangleArea()
+		{
+		int x = 0;
+		
+		Console.WriteLine("Press 1 to use Gerone formula OR press 2 to use other formula:");
+			// ввести readline
+			switch (x) {
+			case 1:
+			Console.WriteLine("Переменная равна 1");
+			Console.ReadLine();
+			// тут ввести 3 числа і реалізація ретурн
+			break; 
+			case 2: 
+			Console.ReadLine();
+			Console.WriteLine("Переменная равна 2");
+			// тут ввести 2 числа і реалізація ретурн
+			break;
+			default:
+			Console.WriteLine("Что-то другое");
+			break;
+			}
 		}
 	}
 }
