6fdbf71a3ac7e3b1f39a51804e7897efdeae72aa
nazar.boichuk-at-934627200577 <nboichuk@softserveinc.com>
2021-07-22 15:27:59 +0300

    update

---
+++ RefOutIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RefOutIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//int x = 6;
+			var arr = new int[]{ 1, 2, 3, 4, 5 };
+
+			var x = "r";
+
+			var y = '%';
+			char w = '^';
+			byte t = 255;
+			int r = t;
+			object o = r;
+
+			int i = (int)o;
+
+			long b = (int)o;
+
+			//short s = (short)r;
+
+			//Console.WriteLine(x);
+			//Console.WriteLine(MethodToUseRef(ref x));
+			PrintNumber(5, c:5);
+
+			
+		}
+
+		//public static int MethodToUseRef(ref int x)
+		//{
+		//	x = 5;
+		//	return x;
+		//}
+
+		public static void PrintNumber(int a, int b = 5, int c = 6)
+		{
+
+		}
+
+		public static int PrintNumber(int d)
+		{
+			return d;
+		}
+	}
+}
--- TriangleAreaCalculate2.0/Program.cs
+++ TriangleAreaCalculate2.0/Program.cs
@@ -8,24 +8,26 @@ namespace TriangleAreaCalculator
 		{
 			// створити консольну аплікацію, ка обчислює площу трикутника 2 способами:
 			// формула Герона, і формула 1/2 висоти трикутника помножена сторону до якої опущена
-			CalculateTriangleArea(8, 20, 0, 0);
+			double R = 0;
+			CalculateTriangleArea(H:20, A:20, 0, 0, out R);
 		}
 
-		public static void CalculateTriangleArea(double H, double A, double B, double C)
+		public static void CalculateTriangleArea(double H, double A, double B, double C, out double S)
 		{
 			Console.WriteLine("************** Enter which way you want to use ***************");
-			Console.WriteLine("To use Gerone - type 1, use second way - type 2:");
+			Console.WriteLine("To use Gerone - type 2, use second way - type 1:");
 			string nameOfType = Console.ReadLine();
+			S = 0;
 			switch (nameOfType)
 			{
 				case "1":
-					double S = (A * H) / 2;
+					S = (A * H) / 2;
 					Console.WriteLine(S);
 					break;
 				case "2":
 					double P = (A + B + C) / 2;
-					double S1 = Math.Sqrt(P * (P - A) * (P - A) * (P - A));
-					Console.WriteLine(S1);
+					S = Math.Sqrt(P * (P - A) * (P - A) * (P - A));
+					Console.WriteLine(S);
 					break;
 				default:
 					Console.WriteLine("***** not found ******");
