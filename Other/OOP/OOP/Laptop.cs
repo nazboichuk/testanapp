3a839173a1351a9b5595db557dfb286c15ee7b0f
nazar.boichuk-at-934627200577 <nboichuk@softserveinc.com>
2021-08-06 17:44:25 +0300

    update2

--- Other/OOP/OOP/Laptop.cs
+++ Other/OOP/OOP/Laptop.cs
@@ -4,46 +4,46 @@ using System.Text;
 
 namespace OOP
 {
-	// Working with value type
 	public struct GPU
 	{
-		public int GpuRam;
+		public int gpuRam;
 
 		public string GetInfoGpu()
 		{
-			return $"We have the {GpuRam} RAM of the videocard in the laptop.";
+			return $"We have the {gpuRam} RAM of the videocard in the laptop.";
 		}
 	}
 
 	public struct CPU
 	{
-		public double CpuFrequency;
+		public double cpuFrequency;
 
 		public string GetInfoCpu()
 		{
-			return $"We have the {CpuFrequency} processor frequency in the laptop.";
+			return $"We have the {cpuFrequency} processor frequency in the laptop.";
 		}
 	}
-
-	// Working with reff type
+	
 	public class Laptop
 	{
 		public string manufacturer;
 		public string model;
 		public int ram;
 		public int hdd;
+		public CPU cpuFrequency;
+		public GPU gpuRam;
 
-		public Laptop() // Default contstructor
+		public Laptop()
 		{
 		}
 
-		public Laptop(int ram, int hdd) // Parts only
+		public Laptop(int ram, int hdd)
 		{
 			this.ram = ram;
 			this.hdd = hdd;
 		}
-
-		public Laptop(string manufacturer, string model) // Brand/model only
+		
+		public Laptop(string manufacturer, string model)
 		{
 			this.manufacturer = manufacturer;
 			this.model = model;
@@ -63,11 +63,11 @@ namespace OOP
 			return this;
 		}
 
-		public void ViewLaptopInfo() // Print all changes
+		public void ViewLaptopInfo()
 		{
 			Console.WriteLine($"The RAM is {ram}, GB and the hard drive is {hdd} TB.");
 			Console.WriteLine($"The brand is {manufacturer}, and model is {model}.");
-			//Console.WriteLine($"The brand {Gp} and model is {model}");
+			Console.WriteLine($"The frequency of the CPU is {cpuFrequency}, and memory for the graphics card is {gpuRam} GB.");
 		}
 
 	}
--- Other/OOP/OOP/Program.cs
+++ Other/OOP/OOP/Program.cs
@@ -11,18 +11,18 @@ namespace OOP
 			myLaptop.ChangeLaptopParts(64, 2);
 			myLaptop.ChangeLaptopType("Acer", "Aspire");
 
+			
 			// Change the GPU of the videocard
-			var myGpu = new GPU();
-			myGpu.GpuRam = 16;
+			// var myGpu = new GPU();
+			// myGpu.gpuRam = 16;
 
 			// Change the frequency of the CPU
-			var myCpu = new CPU();
-			myCpu.CpuFrequency = 2.4;
-
-			// View the details
+			// var myCpu = new CPU();
+			// myCpu.cpuFrequency = 2.4;
+			
 			myLaptop.ViewLaptopInfo();
-			Console.WriteLine(myCpu.GetInfoCpu());
-			Console.WriteLine(myGpu.GetInfoGpu());
+			//Console.WriteLine(myCpu.GetInfoCpu());
+			//Console.WriteLine(myGpu.GetInfoGpu());
 		}
 	}
 }
