a1e6049b54c6bf3b6a27efbafd4f27fbf47c43ae
nazar.boichuk-at-934627200577 <nboichuk@softserveinc.com>
2021-08-15 20:41:23 +0300

    CSVHelper error

---
+++ Other/CsvHelper/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetCsvData
+{
+	[Serializable]
+	public class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+	}
+}
--- Other/CsvHelper/Program.cs
+++ Other/CsvHelper/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+using System.Linq;
 
-namespace CsvHelper
+namespace GetCsvData
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new Person();
+			using (var reader = new StreamReader(@"C:\Users\nboichuk\Documents\GitHub\testanapp\Other\CsvHelper\data\file.csv"))
+			{
+				using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+				{
+					var result = csv.GetRecords<Person>();
+					person = result.First();
+				}
+			}
+			Console.WriteLine(person.FirstName + person.LastName + person.Age);
+		}
+	}
 }
