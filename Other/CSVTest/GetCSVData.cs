ca550bb9b3d6afbe1159cd9c4542204b2d5e9441
nazar.boichuk-at-934627200577 <nboichuk@softserveinc.com>
2021-08-17 17:46:32 +0300

    Person CSV Helper task

---
+++ Other/CSVTest/GetCSVData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+using System.Linq;
+using PersonProperties;
+
+namespace GetCsvData
+{
+	public static class PersonCsvHelper
+	{
+		public static void PersonData()
+		{
+				Person person = new Person();
+			using (var reader = new StreamReader(@"C:\Users\nboichuk\Documents\GitHub\testanapp\Other\CSVTest\data\file.csv"))
+			{
+					using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+					{
+						var result = csv.GetRecords<Person>();
+						person = result.First();
+					}
+				}
+			Console.WriteLine($"1-st sell is: {person.FirstName}, next one is: {person.LastName}, and age is: {person.Age}");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Other/CSVTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+using System.Linq;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GetCsvData.PersonCsvHelper.PersonData();
+
+		}
+	}
+}
